d623d324107f9ea9edec0ed25227255d9309518b
JardinierBorgne <nathan.boel@live.fr>
2024-05-28 23:02:18 +0200

    Create Flush_Files.py

---
+++ Flush_Files.py
@@ -0,0 +1,10 @@
+def clear_alert_files():
+    file_names = ['cpu_alerts.txt', 'ram_alerts.txt', 'ip_alerts.txt', 'root_alerts.txt']
+
+    for file_name in file_names:
+        with open(file_name, 'w') as file:
+            file.truncate(0)
+    print("Les fichiers ont été vidés avec succès.")
+
+if __name__ == "__main__":
+    clear_alert_files()
