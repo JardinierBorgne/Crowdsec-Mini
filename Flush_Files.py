9232bc732b8189394affbdf7e1d0fe215d2d292a
JardinierBorgne <nathan.boel@live.fr>
2024-05-29 22:50:29 +0200

    Update Flush_Files.py

--- Flush_Files.py
+++ Flush_Files.py
@@ -1,5 +1,5 @@
 def clear_alert_files():
-    file_names = ['cpu_alerts.txt', 'ram_alerts.txt', 'ip_alerts.txt', 'root_alerts.txt']
+    file_names = ['cpu_alerts.txt', 'ram_alerts.txt', 'ip_alerts.txt', 'root_alerts.txt', 'proc_status_alerts.txt']
 
     for file_name in file_names:
         with open(file_name, 'w') as file:
